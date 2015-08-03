80c1d4da0715c22b48489cad8cae8fc7936202b7
amerriman <amm434@gmail.com>
2015-08-03 11:46:35 -0600

    finished loops

--- js/main.js
+++ js/main.js
@@ -3,3 +3,33 @@
 $(document).on('ready', function() {
   console.log('sanity check!');
 });
+
+
+var students = ['joe', 'carl', 'rachel', 'derek', 'lily'];
+
+//Problem 1
+for (var i = 0; i < students.length - 1; i++){
+  console.log(students[i]);
+}
+
+//Problem 2
+for (var i = 0; i <= students.length; i+=2) {
+  console.log(students[i]);
+}
+
+//Problem 3
+for (var i=students.length-1; i>=0; i--) {
+  console.log(students[i]);
+}
+
+//Problem 4
+for(var i = 0; i < students.length; i++) {
+  if(students[i] === 'joe' || students[i] === 'lily'){
+    console.log(students[i]);
+  }
+    else{
+      for(var j = 0; j < 2; j++) {
+        console.log(students[i]);
+    }
+  }
+}
